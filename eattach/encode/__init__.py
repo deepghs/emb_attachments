0ee608161b4848bd06928820506a1f913c925cf4
narugo1992 <narugo1992@deepghs.org>
2025-03-04 13:58:38 +0800

    dev(narugo): update dircode

--- eattach/encode/__init__.py
+++ eattach/encode/__init__.py
@@ -1,2 +1,3 @@
-from .direncode import encode_for_dir, is_npy_file, UnsupportedFileFormat
+from .direncode import encode_for_dir, is_npy_file, UnsupportedFileFormat, is_npz_file, get_npz_file, \
+    npz_file_key_exists, npz_file_append, get_embedding_key, clean_encode_for_dir
 from .encoder import Encoder, get_encoder, register_encode_system
